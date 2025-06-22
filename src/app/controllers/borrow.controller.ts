4d6c764c10c35b1a3d65a2faeb255d8d77e98378
Khairul Islam <khairul15-4102@diu.edu.bd>
2025-06-22 16:57:27 +0600

    Borrowed Books Summary (Using Aggregation) api

--- src/app/controllers/borrow.controller.ts
+++ src/app/controllers/borrow.controller.ts
@@ -79,3 +79,45 @@ borrowRoutes.post('/', async (req: Request, res: Response, next: NextFunction): 
     }
 );
 
+
+// get Borrowed Books Summary (Using Aggregation) 
+borrowRoutes.get('/', async (req: Request, res: Response, next) => {
+    try {
+        //crete a aggregate to show borrow book information
+        const summary = await Borrow.aggregate([
+            {
+                $group: {
+                    _id: '$book',
+                    totalQuantity: { $sum: '$quantity' }
+                }
+            },
+            {
+                $lookup: {
+                    from: 'books',
+                    localField: '_id',
+                    foreignField: '_id',
+                    as: 'bookInfo'
+                }
+            },
+            { $unwind: '$bookInfo' },
+            {
+                $project: {
+                    _id: 0,
+                    book: {
+                        title: '$bookInfo.title',
+                        isbn: '$bookInfo.isbn'
+                    },
+                    totalQuantity: 1
+                }
+            }
+        ]);
+
+        res.status(200).json({
+            success: true,
+            message: 'Borrowed books summary retrieved successfully',
+            data: summary,
+        });
+    } catch (error) {
+        next(error);
+    }
+});
