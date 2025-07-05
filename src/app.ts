dd88dcbc0681e59550cb559cf2d6e19a4030cb2b
Khairul Islam <khairul15-4102@diu.edu.bd>
2025-07-05 20:01:04 +0600

    added cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,21 @@
 import express, { Application, Request, Response } from "express";
+import cors from "cors"; 
 import { booksRoutes } from "./app/controllers/book.controller";
 import { errorHandler } from "./app/middlewares/errorHandler";
 import { borrowRoutes } from "./app/controllers/borrow.controller";
 
 const app: Application = express();
 
+const corsOptions = {
+  origin: [
+    "https://library-management-client-sage.vercel.app",
+    "http://localhost:5173",
+  ],
+  credentials: true,
+  optionalSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions))
 app.use(express.json());
 
 app.use("/api/books", booksRoutes);
